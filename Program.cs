3ac658fc1ce3b3df0b62845287a8ba5acf06feca
Katherine Beam <k.beam@umiami.edu>
2016-05-01 14:45:57 -0400

    Allow Hangman answer to be changed from command line foreach new instance of the game

--- Program.cs
+++ Program.cs
@@ -8,9 +8,9 @@ namespace HangmanGame
     public class Program
     {
         public static void Main(string[] args)
-        {
+        {   
             //instantiate new Game object
-            Game game = new Game("spaceship");
+            Game game = new Game(args[0]);
             
             //instantiate new Prompter object
             Prompter prompter = new Prompter(game);
